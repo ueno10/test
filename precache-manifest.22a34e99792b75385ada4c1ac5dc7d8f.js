bfa04793236d4a922de7da4268d905a60dc5beca
ueno10 <uenomizuki.nu@gmail.com>
2020-08-14 00:43:59 +0900

    Updates

---
+++ precache-manifest.22a34e99792b75385ada4c1ac5dc7d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f592c8c04e574ef96325ee54e3a89c1",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3f4d46154bc46d832ba5",
+    "url": "/test/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "3f4d46154bc46d832ba5",
+    "url": "/test/static/js/2.e3d542eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.e3d542eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0207acb51c840fb24407",
+    "url": "/test/static/js/main.516d8f79.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
